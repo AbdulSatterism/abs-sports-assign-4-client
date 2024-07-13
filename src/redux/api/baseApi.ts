1da535d7a96230f8b8077b9fbff60419cd923d42
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2024-07-13 17:52:03 +0600

    backend live link

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://abs-sports-assignment-4.vercel.app/api",
+  }),
   tagTypes: ["product"],
   endpoints: (builder) => ({
     getAllProducts: builder.query({
